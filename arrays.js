7073845735348024fabd1345923bad7d7f79f733
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2019-04-27 18:43:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
+  return array.pop()
 }
 function removeElementFromEndOfArray(array) {
   return array.slice(0,array.length - 1)
